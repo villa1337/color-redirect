34eb1c1a0fb2b7af3e0472192bf2c09db5087ed1
francisco <francisco@example.com>
2025-12-25 19:36:09 -0600

    Fix routing with rewrite rule

--- index.js
+++
@@ -1,6 +0,0 @@
-export default function handler(req, res) {
-  const tunnelUrl = process.env.CLOUDFLARE_TUNNEL_URL || 'https://robots-advantages-painted-dealtime.trycloudflare.com';
-  
-  // Simple redirect to the Pi's color site
-  res.redirect(302, tunnelUrl);
-}
